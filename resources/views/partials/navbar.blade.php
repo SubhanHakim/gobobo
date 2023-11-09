f87e8c30b1c3b91cd412fb32fde88c96a533033d
Subhan Hakim <subhanhakim07@gmail.com>
2023-11-09 10:38:38 +0700

    Nambaha baguan

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -12,6 +12,9 @@
                 <li class="nav-item">
                     <a class="nav-link disabaled" href="#">Sign Up</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link disabaled" href="#">Sign Up</a>
+                </li>
             </ul>
         </div>
     </div>
