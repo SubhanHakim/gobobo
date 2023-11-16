76b4e4d653e3ec36ee7dd0dbd3e3d4a11bda052b
Muhammad Ferry <ferryhidayat656@gmail.com>
2023-11-16 13:43:09 +0700

    signin

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,16 +5,26 @@
 <div class="bg-sigin">
     <div class="d-flex image-signin">
         <div class="container-signin">
-            <img src="..\assets\img-signin.png" alt="">
+            <div class="atas">
+                <img class="header-image" src="..\assets\img-signin.png" alt="">
+            </div>
+           
             <div class="centered">
-                <h1 class="text-signin">Login to your account </h1>                
-                <h2 class="text-center">Explore GOBOBO comfortably, you can see which villa will be your holiday choice today </h2>                
+                    <h1 class="text-signin">Login to your account </h1>                
+                    <h2 class="text-center">Explore GOBOBO comfortably, you can see which villa will be your holiday choice today </h2>                
             </div>
         </div>
         <div class="right">
-            <h1 class="text-sigin">
-                SIGN IN
-            </h1>
+                <h1 class="text-sigin">SIGN IN</h1>
+            <div class="form-signin">
+                <form action="/action_page.php">
+                    <label for="fname">USERNAME</label><br>
+                    <input type="text" id="fname" name="fname" value=""><br>
+                    <label for="lname">PASSWORD:</label><br>
+                    <input type="text" id="lname" name="lname" value=""><br><br>
+                    <input type="submit" value="LOGIN">
+                </form> 
+            </div>
         </div>
     </div>
 </div>
